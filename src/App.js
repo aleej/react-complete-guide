30aa7e7564a9d9b479382d34eebbe1559b98dd34
Anthony Lee <anthony_lee@bcbsil.com>
2020-01-21 09:45:55 -0600

    Section 5 Task 74 - complete css modules

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import './App.css';
+
+import classes from './App.css';
 
 import Person from './Person/Person';
 
@@ -34,6 +35,8 @@ class App extends Component {
 
   render() {
     let persons = null;
+    let btnClass = '';
+
 
     if (this.state.showPersons === true ){
       persons = (
@@ -49,25 +52,29 @@ class App extends Component {
           })}
         </div>
       );      
+
+      btnClass = classes.Red;
     }
 
 
-    let classes = [];
+    let assignedClasses = [];
     if (this.state.persons.length <= 2){
-      classes.push('red'); //classes wil be red
+      assignedClasses.push(classes.red); //classes wil be red
     }
     if (this.state.persons.length <= 1){
-      classes.push('bold');
+      assignedClasses.push(classes.bold);
     }
 
 
     return (
-      <div className="App">
-        <h1>Hi! I'm a React App</h1>
-        <p className={classes.join(' ')}>This is really working</p>
+      <div className={classes.App}>
+        <h1>I'm a React App!</h1>
+        <p className={assignedClasses.join(' ')}>This is really working</p>
         <button
-          alt={this.state.showPersons}
-          onClick={this.togglePersonsHandler}>Toggle Persons</button>
+          className={btnClass}
+          onClick={this.togglePersonsHandler}>
+            Toggle Persons
+          </button>
         {persons}
       </div>
     );
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,29 +1,15 @@
 import React from 'react';
-import styled from 'styled-components';
 
-//import './Person.css';
-
-const StyledDiv = styled.div`            
-    border: 1px solid #eee;
-    padding: 16px;
-    margin: 16px auto;
-    box-shadow: 0 2px 3px #ccc;
-    width: 60%;
-    text-align: center;
-    
-    @media (min-width: 500px) {
-        width: 450px;
-    }
-`;
+import classes from './Person.css';
 
 const person = (props) => {
     
     return (
-        <StyledDiv>
+        <div className={classes.Person}>
             <p onClick={props.click}>I'm {props.name} and I'm {props.age} years old!</p>
             <p>{props.children}</p>
             <input type="text" onChange={props.changed} value={props.value} />
-        </StyledDiv>        
+        </div>        
     )
 };
 
