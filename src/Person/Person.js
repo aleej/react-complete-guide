8ee5087cffd2c15f28c9c43eb0b3a332ffcc41b9
aleej <anthony.lee@allstate.com>
2020-01-21 19:15:14 -0600

    Fixes bug to display name by default

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -8,7 +8,7 @@ const person = (props) => {
         <div className={classes.Person}>
             <p onClick={props.click}>I'm {props.name} and I'm {props.age} years old!</p>
             <p>{props.children}</p>
-            <input type="text" onChange={props.changed} value={props.value} />
+            <input type="text" onChange={props.changed} value={props.name} />
         </div>        
     )
 };
